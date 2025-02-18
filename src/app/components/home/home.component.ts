c582467055c1c7656cf31d0c10771c211b090d00
thurzin <thurlessaf@gmail.com>
2025-02-17 22:48:49 -0300

    adjust layout for mobile display

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { NgOptimizedImage } from '@angular/common';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
@@ -17,8 +17,39 @@ import { InfoFooterComponent } from '../info-footer/info-footer.component';
     InfoFooterComponent,
   ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
 
+  constructor() { }
+
+  ngOnInit(): void {
+    if (typeof window !== 'undefined') {
+      this.adjustImageSize();
+      window.addEventListener('resize', this.adjustImageSize.bind(this));
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (typeof window !== 'undefined') {
+      window.removeEventListener('resize', this.adjustImageSize.bind(this));
+    }
+  }
+
+  adjustImageSize(): void {
+    if (typeof window !== 'undefined') {
+      const screenWidth = window.innerWidth;
+      const image = document.querySelector('section.main-section img') as HTMLImageElement;
+
+      if (image) {
+        if (screenWidth < 1080) {
+          image.setAttribute('width', '300');
+          image.setAttribute('height', '300');
+        } else {
+          image.setAttribute('width', '500');
+          image.setAttribute('height', '500');
+        }
+      }
+    }
+  }
 }
