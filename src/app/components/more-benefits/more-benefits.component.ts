cc3f8b103abfdc33b37ac1ed320204ac2ebf942a
thurzin <thurlessaf@gmail.com>
2025-02-18 21:09:08 -0300

    ajustes para mobile

--- src/app/components/more-benefits/more-benefits.component.ts
+++ src/app/components/more-benefits/more-benefits.component.ts
@@ -9,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class MoreBenefitsComponent {
 
+  ngOnInit(): void {
+    if (typeof window !== 'undefined') {
+      this.adjustImageSize();
+      window.addEventListener('resize', this.adjustImageSize.bind(this));
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (typeof window !== 'undefined') {
+      window.removeEventListener('resize', this.adjustImageSize.bind(this));
+    }
+  }
+
+  adjustImageSize(): void {
+    if (typeof window !== 'undefined') {
+      const screenWidth = window.innerWidth;
+      const image = document.querySelector('section.main-section img') as HTMLImageElement;
+
+      if (image) {
+        if (screenWidth < 1080) {
+          image.setAttribute('width', '300');
+          image.setAttribute('height', '300');
+        } else {
+          image.setAttribute('width', '500');
+          image.setAttribute('height', '500');
+        }
+      }
+    }
+  }
+
 }
